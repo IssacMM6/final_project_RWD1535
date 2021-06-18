d059ccdbd490a4144823483ae6162a487b2ad863
IssacMM6 <ass123ssa.iziz@gmail.com>
2021-06-18 18:48:01 +0630

    login fromed add

---
+++ js/nav_control.js
@@ -0,0 +1,31 @@
+let getId = (idName) => {
+  return document.getElementById(idName);
+};
+
+let headerElement = {
+  parent: getId("hearder-element"),
+  openToogleBtn: getId("open-toogle"),
+  closeToogleBtn: getId("close-toogle"),
+};
+
+headerElement.openToogleBtn.addEventListener(
+  "click",
+  function () {
+    headerElement.parent.classList.remove("header-close");
+    headerElement.parent.classList.add("header-open");
+    headerElement.openToogleBtn.classList.add("d-none");
+    headerElement.closeToogleBtn.classList.remove("d-none");
+  },
+  false
+);
+
+headerElement.closeToogleBtn.addEventListener(
+  "click",
+  function () {
+    headerElement.parent.classList.remove("header-open");
+    headerElement.parent.classList.add("header-close");
+    headerElement.openToogleBtn.classList.remove("d-none");
+    headerElement.closeToogleBtn.classList.add("d-none");
+  },
+  false
+);
